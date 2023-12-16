9e6304f4274b65978b09c629f96c6e0bfe8a78ae
Andres Morilla <andresmm4242@gmail.com>
2023-12-16 15:59:50 +0100

    p

--- VYPaCompiler/app/build.gradle.kts
+++ VYPaCompiler/app/build.gradle.kts
@@ -37,7 +37,7 @@ application {
 tasks {
 
     generateGrammarSource{
-        maxHeapSize="1";
+        maxHeapSize="100";
         val pkg = "parser"
         arguments = arguments + listOf("-package", pkg) + listOf("-visitor")
         outputDirectory = file("src/main/java/parser")
