39718cc442bdc21c2fca181093bcf0d59b3d8660
Huu-Tu <huutu.nguyen@edu.esiee.fr>
2023-12-18 18:07:03 +0100

    checktype for PrintFunctionClass

--- VYPaCompiler/app/src/main/java/expression/PrintFunction.java
+++ VYPaCompiler/app/src/main/java/expression/PrintFunction.java
@@ -1,6 +1,16 @@
 package expression;
 
+import tables.SymbolTable;
+
 public class PrintFunction extends FunctionInvokeExpression{
+    @Override
+    public void checkType(SymbolTable st) {
+        if (getArgs().getExpressions().isEmpty()) {
+            throw new RuntimeException("Print function requires at least one parameter!");
+        }
+        getArgs().checkType(st);
+    }
+
     public PrintFunction(ExpressionList arguments) {
         super("print", arguments);
     }
