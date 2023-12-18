98e741c4430113bc8c0f86c842ed1d885cec16a0
Huu-Tu <huutu.nguyen@edu.esiee.fr>
2023-12-18 16:17:23 +0100

    substr

---
+++ VYPaCompiler/app/src/main/java/expression/SubStrFunctionDef.java
@@ -0,0 +1,21 @@
+package expression;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SubStrFunctionDef extends FunctionDef{
+    private static  ParamDefList parameters;
+    static {
+        List<ParamDef> paramList = new ArrayList<>(Arrays.asList(
+                new ParamDef("string", "s"),
+                new ParamDef("int", "from"),
+                new ParamDef("int", "to")
+        ));
+        parameters = new ParamDefList(paramList);
+    }
+    public SubStrFunctionDef() {
+        super(STRING_TYPE, "subStr", parameters, new CodeBlock(Collections.emptyList()));
+    }
+}
